a6ff4899c2c44252e4c1339eb742da006390c265
Jelle De Loecker <jelle@elevenways.be>
2022-06-17 14:49:13 +0200

    Use cave vines for climbable blocks

--- src/main/java/io/github/theepicblock/polymc/api/block/BlockStateProfile.java
+++ src/main/java/io/github/theepicblock/polymc/api/block/BlockStateProfile.java
@@ -81,6 +81,7 @@ public class BlockStateProfile {
     private static final Block[] WATERLOGGED_SLABS = {Blocks.SMOOTH_STONE_SLAB}; // Smooth stone double slabs do not look like regular smooth stone blocks. Therefore, only the waterlogged double slab is available to us.
     private static final Block[] WALL_BLOCKS = {Blocks.COBBLESTONE_WALL, Blocks.MOSSY_COBBLESTONE_WALL, Blocks.BRICK_WALL, Blocks.PRISMARINE_WALL, Blocks.RED_SANDSTONE_WALL, Blocks.MOSSY_STONE_BRICK_WALL, Blocks.GRANITE_WALL, Blocks.STONE_BRICK_WALL, Blocks.NETHER_BRICK_WALL, Blocks.ANDESITE_WALL, Blocks.RED_NETHER_BRICK_WALL, Blocks.SANDSTONE_WALL, Blocks.END_STONE_BRICK_WALL, Blocks.DIORITE_WALL, Blocks.BLACKSTONE_WALL, Blocks.POLISHED_BLACKSTONE_BRICK_WALL, Blocks.POLISHED_BLACKSTONE_WALL, Blocks.COBBLED_DEEPSLATE_WALL, Blocks.POLISHED_DEEPSLATE_WALL, Blocks.DEEPSLATE_TILE_WALL, Blocks.DEEPSLATE_BRICK_WALL};
     private static final Block[] PRESSURE_PLATE_BLOCKS = {Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE, Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE};
+    private static final Block[] CLIMBABLE_BLOCKS = {Blocks.CAVE_VINES, Blocks.WEEPING_VINES, Blocks.TWISTING_VINES};
 
     //FILTERS
     private static final Predicate<BlockState> DEFAULT_FILTER = (blockState) -> blockState != blockState.getBlock().getDefaultState();
@@ -112,6 +113,23 @@ public class BlockStateProfile {
     private static final Predicate<BlockState> DOUBLE_SLAB_FILTER = (blockState) -> blockState.get(SlabBlock.TYPE) == SlabType.DOUBLE;
     private static final Predicate<BlockState> WATERLOGGED_SLAB_FILTER = (blockState) -> blockState.get(SlabBlock.TYPE) == SlabType.DOUBLE && blockState.get(SlabBlock.WATERLOGGED);
     private static final Predicate<BlockState> SLAB_FILTER = (blockState) -> blockState.get(SlabBlock.TYPE) != SlabType.DOUBLE;
+    private static final Predicate<BlockState> CLIMBABLE_FILTER = (blockState) -> {
+
+        if (blockState.getBlock() == Blocks.CAVE_VINES) {
+
+            // Don't use any state with age 0
+            if (blockState.get(Properties.AGE_25) == 0) {
+                return false;
+            }
+
+            // Don't use states with berries (they cause block updates on right click)
+            if (blockState.get(CaveVines.BERRIES) == true) {
+                return false;
+            }
+        }
+
+        return DEFAULT_FILTER.test(blockState);
+    };
 
     // Chorus flowers are full blocks, and have 4 states that can be reused
     private static final Predicate<BlockState> CHORUS_FLOWER_FILTER = (blockState) -> {
@@ -167,6 +185,17 @@ public class BlockStateProfile {
         polyRegistry.registerBlockPoly(block, (input) ->
                 input.with(Properties.POWERED, false).with(Properties.DISARMED,false)
         );
+    };
+    private static final BiConsumer<Block,PolyRegistry> CLIMBABLE_ON_FIRST_REGISTER = (block, polyRegistry) -> {
+
+        if (block == Blocks.CAVE_VINES) {
+            polyRegistry.registerBlockPoly(block, (input) ->
+                    // Force the age to be zero, but let the "berries" be whatever it is
+                    input.with(Properties.AGE_25, 0)
+            );
+        } else {
+            polyRegistry.registerBlockPoly(block, new SimpleReplacementPoly(block.getDefaultState()));
+        }
     };
     private static final BiConsumer<Block,PolyRegistry> SMALL_DRIPLEAF_ON_FIRST_REGISTER = (block, polyRegistry) -> polyRegistry.registerBlockPoly(block, input -> {
         if (input.get(TallPlantBlock.HALF) == DoubleBlockHalf.LOWER) {
@@ -267,6 +296,7 @@ public class BlockStateProfile {
 
     //PROFILES
     public static final BlockStateProfile FULL_BLOCK_PROFILE = combine("full blocks", INFESTED_STONE_SUB_PROFILE, /*TNT_SUB_PROFILE,*/ SNOWY_GRASS_SUB_PROFILE, NOTE_BLOCK_SUB_PROFILE, DISPENSER_SUB_PROFILE, BEEHIVE_SUB_PROFILE, WAXED_COPPER_FULLBLOCK_SUB_PROFILE, JUKEBOX_SUB_PROFILE, DOUBLE_SLAB_SUB_PROFILE, TARGET_BLOCK_SUB_PROFILE, WATERLOGGED_SLAB_SUB_PROFILE);
+    public static final BlockStateProfile CLIMBABLE_PROFILE = new BlockStateProfile("climbable blocks", CLIMBABLE_BLOCKS, CLIMBABLE_FILTER, CLIMBABLE_ON_FIRST_REGISTER);
     public static final BlockStateProfile LEAVES_PROFILE = getProfileWithDefaultFilter("leaves", LEAVES_BLOCKS);
     public static final BlockStateProfile NO_COLLISION_WALL_PROFILE = new BlockStateProfile("empty walls", WALL_BLOCKS, WALL_FILTER, WALL_ON_FIRST_REGISTER);
     public static final BlockStateProfile NO_COLLISION_PROFILE = combine("blocks without collisions", KELP_SUB_PROFILE, SAPLING_SUB_PROFILE, SUGARCANE_SUB_PROFILE, /*CAVE_VINES_SUB_PROFILE,*/ TRIPWIRE_SUB_PROFILE, SMALL_DRIPLEAF_SUB_PROFILE, OPEN_FENCE_GATE_PROFILE, PRESSURE_PLATE_PROFILE);
